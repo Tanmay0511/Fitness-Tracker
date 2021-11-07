6e813e852081632007c03c65c72c108f4880d517
hrp141020 <hrp1410@icloud.com>
2021-11-07 13:31:55 -0500

    routes.rb update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  match 'weight_logs/index', to: 'weight_logs#index', via: [:get, :post]
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
-  root 'dashboard#index'
+ root 'dashboard#index'
+ Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
