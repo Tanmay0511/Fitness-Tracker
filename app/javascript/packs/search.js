21fe4cfa3f038fb6fa64ee186d9a0cd9494d74ae
Akhilmaruthi9819 <vpendela@albany.edu>
2021-12-03 14:44:30 -0500

    commit

---
+++ app/javascript/packs/search.js
@@ -0,0 +1,6 @@
+document.addEventListener("turbolinks: load", function() {
+    var options = {
+      data: ["John", "Paul", "George", "Ringo"]
+    };
+    $('[data-behavior="autocomplete"]').easyAutocomplete(options);
+  }); 
